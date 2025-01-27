40b0e256ba75c7549b8ce1883ec70f2c0a46c993
tee101 <akpata.thelma@gmail.com>
2025-01-27 14:43:10 +0100

    type casting redo

--- taskade/Phase1/src/main.ts
+++ taskade/Phase1/src/main.ts
@@ -1,4 +1,19 @@
-import { MatcherFunction } from "@testing-library/react";
+type One = string
+type Two = string | number
+type Three = 'hello'
+
+let e = <string | number>'hello2'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number |
+    string => {
+        if (c == 'add') return a + b
+        return '' + a + b
+}
+
+let myVal: string = addOrConcat(2,2, 'concat') as string
+let nextVal: number = addOrConcat(2,2, 'concat') as number
+
+ 
 
 type stringOrNumber = string | number
 
@@ -10,12 +25,12 @@ type Guitarist = {
     albums: (string | number)[]
 }
 
-type UserId = stringOrNumber 
+type UserId = stringOrNumber
 
 // Literal types
 let myName = "Tee"
 
-let userName: 'Dave' | 'Rachel' |'John'
+let userName: 'Dave' | 'Rachel' | 'John'
 userName = 'Rachel'
 
 // Functions
@@ -28,7 +43,7 @@ const logMsg = (message: any): void => {
 }
 
 logMsg('Hello!')
-logMsg(add(2,39))
+logMsg(add(2, 39))
 
 let subtract = function (c: number, d: number): number {
     return c - d
@@ -40,7 +55,7 @@ let multiply: mathFunction = function (c, d) {
     return c * d
 }
 
-logMsg(multiply(2,2))
+logMsg(multiply(2, 2))
 
 const addAll = (a: number, b: number, c?: number): number {
     if (typeof c !== 'undefined') {
@@ -53,4 +68,4 @@ const total = (...nums: number[]): number => {
     return nums.reduce((prev, curr) => prev + curr)
 }
 
-logMsg(total(1,2,3,4,5,6,7,8,9,10)) 
+logMsg(total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) 
